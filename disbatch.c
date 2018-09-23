6fc235ad9dbb96346f65eb1fb1170fb456715ae8
Allison Bolen <allisonbolen@Allisons-MBP.attlocal.net>
2018-09-23 18:09:28 -0400

    input

---
+++ disbatch.c
@@ -0,0 +1,20 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+  int countRec, countSrv;
+  char input[256];
+  while(1){
+
+    // get user input
+    gets("What File would you like to access: %s", input);
+
+
+  }
+  return 0;
+}
