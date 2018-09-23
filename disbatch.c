c0d664b37f7c51e2a94bba4f00220f548d0393c1
Allison Bolen <allisonbolen@Allisons-MBP.attlocal.net>
2018-09-23 18:47:04 -0400

    thread create

--- disbatch.c
+++ disbatch.c
@@ -7,18 +7,40 @@
 #include <stdio.h>
 
 
-
+void* getFile(void* arg);
 int main()
 {
   int countRec, countSrv;
-  char input[256];
+  char input[256
+
   while(1){
 
     // get user input for threads
     printf("What file would you like to access:");
-
     fgets(input, 256, stdin);
+
+    // create a thread
+    if ((status = pthread_create (&thread1, NULL,  getFile, &input)) != 0) {
+        fprintf (stderr, "thread create error %d: %s\n", status, strerror(status));
+        exit (1);
+    }
+
 
   }
   return 0;
+}
+void* getFile(void* arg){
+  // sleep function
+  srand(rand());
+  char *userInput = (char *) arg;
+  printf("Searching for: %s", userInput);
+  int random rand() % 9;
+  if(random <= 7){
+    // we "read" a file %80 of the time
+    sleep(1);
+  }else{
+    // we block because we are getting the file 20% of the time
+    sleep((rand()%7)+3);
+  }
+  print("Accessed: %s", userInput);
 }
