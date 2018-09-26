b55c1c75f6393e29f62b07d013075419df9b262d
Allison Bolen <allisonbolen@Allisons-MBP.attlocal.net>
2018-09-25 22:32:27 -0400

    fixed memory thing??

--- disbatch.c
+++ disbatch.c
@@ -36,7 +36,7 @@ int main()
     // get user input for threads
     printf("What file would you like to access: ");
     fgets(input[threadCount], 256, stdin);
-    while(strcmp(&input[threadCount], "")){
+    while(strcmp(input[threadCount], "")){
       printf("\n Wont accept blank input!!! try again ");
       fgets(input[threadCount], 256, stdin);
     }
